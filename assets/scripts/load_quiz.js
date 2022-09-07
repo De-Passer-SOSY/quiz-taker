13b19618f766c309fc3441d1c393c479726fb2cc
Kasper103 <90032009+Kasper103@users.noreply.github.com>
2022-09-07 11:50:28 +0200

    Created loading functionality

--- assets/scripts/load_quiz.js
+++ assets/scripts/load_quiz.js
@@ -1,8 +1,18 @@
 
 function loadButton() {
-	
+	document.querySelector("#open-quiz-button").addEventListener("click", open);
 }
 
 function open() {
+	const fileInput = document.createElement("input")
+	fileInput.type = "file";
+	fileInput.click();
 
+	fileInput.addEventListener("change", function() {
+		const reader = new FileReader();
+		reader.onload = function() {
+			exercises = JSON.parse(reader.result);
+		}
+		reader.readAsText(fileInput.files[0]);
+	});
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
+let exercises;
+
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-	
+
 }
