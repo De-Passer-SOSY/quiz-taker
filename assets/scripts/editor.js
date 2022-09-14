f6c8f6d993c67d5f7287b42439ea64e81ade2561
Kasper103 <90032009+Kasper103@users.noreply.github.com>
2022-09-14 11:54:45 +0200

    List of exercises shown in editor

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -1,3 +1,20 @@
 function initPage() {
+	loadExercisesList();
+	document.querySelector("#create-exercise-button").addEventListener("click", createExercise);
+}
 
+function loadExercisesList() {
+	const exercisesList = document.querySelector("#exercises-list");
+	for(const exercise of exercises) {
+		const question = exercise.question;
+		exercisesList.innerHTML += `<li>${question}</li>`;
+	}
+}
+
+function createExercise() {
+	exercises.push({
+		question: "New question",
+		answers: [],
+	});
+	loadExercisesList();
 }
