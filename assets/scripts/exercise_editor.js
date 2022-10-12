4f50c962a1505110819f04125af5a15e1837d0b7
Kasper103 <90032009+Kasper103@users.noreply.github.com>
2022-10-12 10:37:40 +0200

    Save button functional in exercise editor

--- assets/scripts/exercise_editor.js
+++ assets/scripts/exercise_editor.js
@@ -1,17 +1,32 @@
 function initPage() {
 	fillForm();
+	document.querySelector("#edit-exercise-form").addEventListener("submit", processForm);
 }
 function fillForm() {
-	const exerciseIndex = localStorage.getItem("editing-exercise")
-	if(exerciseIndex === null) {
+	const exerciseIndexStr = localStorage.getItem("editing-exercise")
+	if(exerciseIndexStr === null) {
 		return;
 	}
-	const exercise = exercises[parseInt(exerciseIndex)];
+	const exercise = exercises[parseInt(exerciseIndexStr)];
 	document.querySelector("#question").value = exercise.question;
 	document.querySelector("#given").value = exercise.given;
 	document.querySelector("#correct").value = exercise.correct;
 }
 function processForm(e) {
 	e.preventDefault();
+	let exerciseIndex = localStorage.getItem("editing-exercise")
+	if(exerciseIndex === null) {
+		return;
+	}else{
+		exerciseIndex = parseInt(exerciseIndex);
+	}
+	const exercise = {
+		question: document.querySelector("#question").value,
+		given: document.querySelector("#given").value,
+		correct: document.querySelector("#correct").value,
+	};
+	exercises[exerciseIndex] = exercise;
+	localStorage.setItem("quiz", JSON.stringify(exercises));
 
+	window.open("quiz_editor.html", "_self");
 }
