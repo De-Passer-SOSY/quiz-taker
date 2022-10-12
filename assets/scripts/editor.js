24315a49b68686f3bec3ec960fd338b609d26281
Kasper103 <90032009+Kasper103@users.noreply.github.com>
2022-10-12 10:50:47 +0200

    Added delete button for exercises

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -8,7 +8,7 @@ function loadExercisesList() {
 	let index = 0;
 	for(const exercise of exercises) {
 		const question = exercise.question;
-		exercisesHTML += "<li>" + question + " | <button id='exercise-" + index + "'>Edit</button></li>";
+		exercisesHTML += "<li>" + question + " | <button id='exercise-" + index + "'>Edit</button><button id='delete-" + index + "'>X</button></li>";
 		index++;
 	}
 	document.querySelector("#exercises-list").innerHTML = exercisesHTML;
@@ -16,12 +16,24 @@ function loadExercisesList() {
 		document.querySelector("#exercise-" + i).addEventListener("click", function () {
 			editExercise(i);
 		});
+		document.querySelector("#delete-" + i).addEventListener("click", function () {
+			deleteExercise(i);
+		});
 	}
 }
 
 function editExercise(index) {
 	localStorage.setItem("editing-exercise", "" + index);
 	window.open("exercise_editor.html", "_self");
+}
+
+function deleteExercise(index) {
+	if(confirm("Are you sure you want to delete exercise " + (index + 1) + "?")) {
+		localStorage.removeItem("editing-exercise");
+		exercises.splice(index, 1);
+		localStorage.setItem("quiz", JSON.stringify(exercises));
+		loadExercisesList();
+	}
 }
 
 function createExercise() {
